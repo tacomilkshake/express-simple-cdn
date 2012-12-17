07066b2549f8e9fea073fd10408c3322bd92247d
Jamie Steven <jsteven@sirris.com>
2012-12-16 16:34:04 -0800

    Preparing for 1.0.

--- express-easycdn.js
+++ express-easycdn.js
@@ -2,68 +2,18 @@
 // Copyright (c) 2012 Jamie Steven, jamiesteven on github.
 // Licensed under MIT
 
+var crc = require('crc');
+
 getBucket = function(string, numbuckets) {
-    var bucket, hash;
-    hash = crc32(string);
-    bucket = hash % numbuckets;
+    var hash = crc.crc8(string);
+    var bucket = hash % numbuckets;
     return bucket;
 };
 
-crc32 = function(s) {
-    var c, crc, finalXORValue, i, initialValue, j, polynomial, reverse, table;
-    reverse = function(x, n) {
-        var b;
-        b = 0;
-        while (n) {
-            b = b * 2 + x % 2;
-            x /= 2;
-            x -= x % 1;
-            n--;
-        }
-        return b;
-    };
-    s = String(s);
-    polynomial = 79764919;
-    initialValue = 4294967295;
-    finalXORValue = 4294967295;
-    crc = initialValue;
-    table = [];
-    i = void 0;
-    j = void 0;
-    c = void 0;
-    i = 255;
-    while (i >= 0) {
-        c = reverse(i, 32);
-        j = 0;
-        while (j < 8) {
-            c = (c * 2 ^ (c >>> 31) % 2 * polynomial) >>> 0;
-            j++;
-        }
-        table[i] = reverse(c, 32);
-        i--;
-    }
-    i = 0;
-    while (i < s.length) {
-        c = s.charCodeAt(i);
-        if (c > 255) {
-            throw new RangeError();
-        }
-        j = crc % 256 ^ c;
-        crc = (crc / 256 ^ table[j]) >>> 0;
-        i++;
-    }
-    return (crc ^ finalXORValue) >>> 0;
-};
-
-var CDN = function(path, cdn, httpsCdn) {
-    if (httpsCdn) {
-        if (httpsCdn.length) {
-            return httpsCdn[getBucket(path, httpsCdn.length)] + path;
-        } else {
-            return httpsCdn + path;
-        }
-    } else if (cdn) {
-        if (cdn.length) {
+var CDN = function(path, cdn) {
+    var d = new Date();
+    if (cdn) {
+        if (cdn instanceof Array) {
             return cdn[getBucket(path, cdn.length)] + path;
         } else {
             return cdn + path;
